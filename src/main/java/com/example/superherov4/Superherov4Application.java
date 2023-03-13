56420201559948fe5c002fe8a1e847381c47b208
heva0011 <113143767+heva0011@users.noreply.github.com>
2023-03-13 20:19:03 +0100

    final commit

---
+++ src/main/java/com/example/superherov4/Superherov4Application.java
@@ -0,0 +1,12 @@
+package com.example.superherov4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Superherov4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Superherov4Application.class, args);
+    }
+}
---
+++ src/test/java/com/example/superherov4/Superherov4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.superherov4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Superherov4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
